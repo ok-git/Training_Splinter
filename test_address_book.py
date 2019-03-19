2506494764c4572ed6ba6fa9403148885a2ecf16
Oleg <olka.contact@gmail.com>
2019-03-19 18:04:19 +0700

    Добавил test_add_addressbook_in_mh на две книги с ожиданями

--- test_address_book.py
+++ test_address_book.py
@@ -1,7 +1,7 @@
 from splinter import Browser
 
 
-def test_add_group():
+def test_add_group_addressbook():
     with Browser() as browser:
         # open baseurl
         browser.visit('http://localhost/addressbook')
@@ -22,4 +22,37 @@ def test_add_group():
         # open groups page
         browser.click_link_by_partial_href('group.php')
         assert browser.is_text_present('splinter_group')
+
+
+def test_add_addressbook_in_mh():
+    with Browser() as browser:
+        # open url
+        browser.visit('https://bulk.boomware.com/login')
+        # login
+        browser.fill('email', 'test@svyazcom.ru')
+        browser.fill('password', '123456Test')
+        browser.find_by_tag('button').click()
+        # enter token
+        browser.fill('code', '978896')
+        browser.find_by_tag('button').click()
+        # open AB page
+        browser.click_link_by_partial_href('address_book')
+        # init AB creation
+        browser.find_by_css('button.btn.btn-primary.modal_add').click()
+        # fill AB form
+        browser.fill('name', 'Splinter book15')
+        # submit AB creation
+        browser.find_by_xpath("//button[@type ='submit']").click()
+        browser.is_element_not_present_by_css('div.modal-dialog', wait_time=5)
+        # init AB creation
+        browser.find_by_css('button.btn.btn-primary.modal_add').click()
+        # fill AB form
+        browser.fill('name', 'Splinter book16')
+        # submit AB creation
+        browser.find_by_xpath("//button[@type ='submit']").click()
+        browser.is_element_not_present_by_css('div.modal-dialog', wait_time=5)
+        # logout
+        browser.find_by_css('li.dropdown.user').click()
+        browser.click_link_by_partial_href('logout')
+    pass
 
