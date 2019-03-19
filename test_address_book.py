da14cfa86ce683eddde38e71ae8b24ff3aa8e97b
Oleg <olka.contact@gmail.com>
2019-03-19 14:40:26 +0700

    Добавил провекри в test_add_group

--- test_address_book.py
+++ test_address_book.py
@@ -11,5 +11,11 @@ def test_add_group():
         browser.find_by_value('Login').click()
         # open groups page
         browser.click_link_by_partial_href('group.php')
+        browser.find_by_name('new').click()
+        browser.fill('group_name', 'splinter_group')
+        browser.fill('group_header', 'splinter_header')
+        browser.fill('group_footer', 'splinter_footer')
+        browser.find_by_name('submit').click()
+        browser.click_link_by_partial_href('group.php')
+        assert browser.is_text_present('splinter_group')
 
-        pass
